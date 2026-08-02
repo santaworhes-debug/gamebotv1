495d95923fda139c82777e7b97edf141b6e1147b
santaworhes-debug <santaworhes@gmail.com>
2026-08-02 07:58:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import asyncio
 import logging
-from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, ConversationHandler
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler
 from handlers.battle_handlers import battle_handler, attack, defend, flee
 from handlers.profile_handlers import start_handler, profile_handler
 from database import Database
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 class GameBot:
@@ -20,28 +19,13 @@ class GameBot:
         self.app.add_handler(CommandHandler("start", start_handler))
         self.app.add_handler(CommandHandler("profile", profile_handler))
         self.app.add_handler(CommandHandler("battle", battle_handler))
-        self.app.add_handler(CommandHandler("inventory", inventory_handler))
-        self.app.add_handler(CommandHandler("shop", shop_handler))
-        self.app.add_handler(CallbackQueryHandler(self._callback_router))
-
-    async def _callback_router(self, update, context):
-        query = update.callback_query
-        await query.answer()
-        data = query.data.split(":")
-        route_map = {
-            "attack": battle_handler.attack,
-            "defend": battle_handler.defend,
-            "use_item": inventory_handler.use_item,
-            "buy": shop_handler.buy_item,
-            "sell": shop_handler.sell_item
-        }
-        handler = route_map.get(data[0])
-        if handler:
-            await handler(update, context)
+        self.app.add_handler(CallbackQueryHandler(attack, pattern="attack"))
+        self.app.add_handler(CallbackQueryHandler(defend, pattern="defend"))
+        self.app.add_handler(CallbackQueryHandler(flee, pattern="flee"))
 
     def run(self):
-        logger.info("GameBot запущен. PID: %d", os.getpid())
-        self.app.run_polling(allowed_updates=["message", "callback_query"])
+        logger.info("GameBot запущен")
+        self.app.run_polling()
 
 if __name__ == "__main__":
     bot = GameBot()
